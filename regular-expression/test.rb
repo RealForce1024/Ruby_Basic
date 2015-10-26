0d234888d8f070f50aa552def2fca5dcaf1fff1c
fqc <337940626@qq.com>
2015-10-26 08:24:20 +0800

    简单匹配邮箱域名及用户名

--- regular-expression/test.rb
+++ regular-expression/test.rb
@@ -1,4 +1,30 @@
-AAA
-AA
-A
-A{1,3}
+# Ruxin.Jiang@126.com
+# Zijiang.Wang@163.com
+# Kangyong.Cai@hotmail.com
+
+emails = %w[Ruxin.Jiang@126.com Zijiang.Wang@163.com Kangyong.Cai@hotmail.com]
+re = "^(.*)@(.*)$"
+emails.each { |e|
+  match = Regexp.compile(re).match(e)
+  puts "email= #{e}"
+  puts "username = #{$1}"
+  puts "company = #{$2}"
+  puts "*******************"
+}
+#简单实现了下...还需要完善...
+#打印结果如下
+puts_result = <<doc
+email= Ruxin.Jiang@126.com
+username = Ruxin.Jiang
+company = 126.com
+*******************
+email= Zijiang.Wang@163.com
+username = Zijiang.Wang
+company = 163.com
+*******************
+email= Kangyong.Cai@hotmail.com
+username = Kangyong.Cai
+company = hotmail.com
+*******************
+doc
+
